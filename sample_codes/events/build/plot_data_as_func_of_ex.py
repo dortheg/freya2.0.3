25d98817c25d250b1516f3827a86ea3ab7bcce86
Dorthea Gjestvang <dorthea.gjestvang@gmail.com>
2018-03-12 14:00:22 +0100

    Plot total energy

--- sample_codes/events/build/plot_data_as_func_of_ex.py
+++ sample_codes/events/build/plot_data_as_func_of_ex.py
@@ -7,6 +7,7 @@ unc = np.loadtxt('uncertainties.dat', skiprows=0, usecols=(0,1,2))
 Ex = data[:,4]
 avg_ph_mult = data[:,5]
 avg_ph_energy = data[:,7]
+total_ph_E = data[:,9]
 
 #plt.plot(Ex, avg_ph_mult, 'o')
 plt.figure(0)
@@ -17,8 +18,15 @@ plt.ylabel("Average photon multiplicity")
 #plt.show()
 
 plt.figure(1)
-plt.errorbar(Ex, avg_ph_mult, yerr=unc[:,1], fmt="x")
+plt.errorbar(Ex, avg_ph_energy, yerr=unc[:,1], fmt="x")
 plt.title("Average photon energy as function of Pu241 Ex-energy")
+plt.xlabel("Excitation energy of Pu241*")
+plt.ylabel("Average photon energy")
+#plt.show()
+
+plt.figure(2)
+plt.errorbar(Ex, total_ph_E, yerr=unc[:,2], fmt="x")
+plt.title("Total photon energy as function of Pu241 Ex-energy")
 plt.xlabel("Excitation energy of Pu241*")
 plt.ylabel("Average photon energy")
 plt.show()
