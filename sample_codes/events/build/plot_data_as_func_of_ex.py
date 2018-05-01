2fcfb92bd73dd2a85da6de62c6b8e8f4c38e380b
Dorthea Gjestvang <dorthea.gjestvang@gmail.com>
2018-05-01 13:17:08 +0200

    Brought Changes up to date, fixed so that include pre-fission neutron plotting, else hopw everything else is up to date. If not, copy of build folder lies in Applications

--- sample_codes/events/build/plot_data_as_func_of_ex.py
+++ sample_codes/events/build/plot_data_as_func_of_ex.py
@@ -2,9 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt 
 
 data = np.loadtxt('data_as_func_of_excitation_energy.dat.unchanged', skiprows=0, usecols=(0,1,2,3,4,5,6,7,8,9,10))
-unc = np.loadtxt('uncertainties.dat', skiprows=0, usecols=(0,1,2))
+#unc = np.loadtxt('uncertainties.dat', skiprows=0, usecols=(0,1,2))
+n0 = np.loadtxt('pre-fission-neutrons.dat', skiprows=0, usecols=(0,1,2))
 
-Ex = data[:,4]
+#Ex = data[:,4]
+Ex = [0,1,2,3,4,5,5.25,5.5,5.75,6,6.25,6.5,6.75,7,7.25,7.5,8.0]
+"""
 avg_ph_mult = data[:,5]
 avg_ph_energy = data[:,7]
 total_ph_E = data[:,9]
@@ -32,5 +35,18 @@ plt.plot(Ex, total_ph_E, "o")
 plt.title("Total photon energy as function of Pu241 Ex-energy")
 plt.xlabel("Excitation energy of Pu241*")
 plt.ylabel("Average photon energy")
+plt.show()
+"""
+plt.figure(3)
+plt.plot(Ex, n0[:,0], '-o')
+plt.plot(Ex, n0[:,1], '-o')
+plt.plot(Ex, n0[:,2], '-o')
+plt.title("Avg number of pre-fission neutrons as function of Pu241 Ex-energy")
+plt.axis([0,max(Ex),0,2.5])
+#plt.xticks(np.arange(min(Ex), max(Ex), 0.5)) #makes ticks alog x-axis
+#plt.yticks(np.arange(min(n0[:,0]), max(n0[:,0]), 0.1)) #ticks along y-axis
+plt.grid(True)
+plt.xlabel("Excitation energy of Pu241*")
+plt.ylabel("Avg number of pre-fission neutrons")
 plt.show()
 
